889d50efcf17c885b3a078768a9d17269ca2f481
develqhy <ikettle9000@163.com>
2024-03-06 13:27:01 +0800

    项目初始化环境搭建

---
+++ xuecheng-online-content/xuecheng-online-content-api/src/main/java/com/xconline/content/ServiceContentApplication.java
@@ -0,0 +1,17 @@
+package com.xconline.content;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: qhy
+ * @date: 2024-03-06 13:07
+ * @description: 内容管理服务
+ */
+@SpringBootApplication
+public class ServiceContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceContentApplication.class, args);
+    }
+}
